0c730a5735e3b5c9c6029a2ae7f713069205009c
Felippe <felippeotaviorosa@gmail.com>
2020-08-19 22:00:41 -0300

    Primeiro programa java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello,world");
+		System.out.print("Esta eh uma saudacao");
+		System.out.printf("Idade: %d",29);
+	}
+}
